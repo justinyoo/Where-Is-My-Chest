d6ce8c893df6ac195622244a9724ef799c655dc8
Justin Yoo <justin.yoo@aliencube.com>
2016-11-28 00:41:36 +1100

    Update app.js

--- src/WhereIsMyChest/wwwroot/js/app.js
+++ src/WhereIsMyChest/wwwroot/js/app.js
@@ -7,17 +7,37 @@ var vm = new Vue({
     "chests": chests
   },
   "computed": {
-    "sequences": function() {
-      var seqs = this.sequence.split("");
+    "sequences": function () {
+      var resultsA = [];
 
-      var results = [];
-      for (var i = 0; i < seqs.length; i++) {
-        var seq = seqs[i];
-        var result = new jinqJs().from(chests).where(function (chest) { return chest.value === seq; }).top(1).select();
-        results.push(result);
+      if (this.sequence.length < 3) {
+        return resultsA;
       }
 
-      return results;
+      var chestSeqs = "";
+      var chestSeqValues = new jinqJs().from(this.chests).select("value");
+      for (var i = 0; i < chestSeqValues.length; i++) {
+        chestSeqs += chestSeqValues[i].value;
+      }
+
+      var inputSeqs = this.sequence.replace(/[XUEL]/g, ".");
+
+      var re = new RegExp(inputSeqs, "g");
+      var possibleSeqs = chestSeqs.match(re);
+
+      for (var i = 0; i < possibleSeqs.length; i++) {
+        var resultsB = [];
+        var seqs = possibleSeqs[i];
+        for (var j = 0; j < seqs.length; j++) {
+          var seq = seqs[j];
+          var result = new jinqJs().from(this.chests).where(function (chest) { return chest.value === seq; }).top(1).select();
+          resultsB.push(result[0]);
+        }
+
+        resultsA.push(resultsB);
+      }
+
+      return resultsA;
     }
   }
 });
