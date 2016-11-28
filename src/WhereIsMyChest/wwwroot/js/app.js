2db98a66859fdfcd2b931b94e7294607d453daed
Justin Yoo <justin.yoo@aliencube.com>
2016-11-29 09:56:30 +1100

    Refactor app.js

--- src/WhereIsMyChest/wwwroot/js/app.js
+++ src/WhereIsMyChest/wwwroot/js/app.js
@@ -1,5 +1,59 @@
 ﻿"use strict";
 
+class Chest {
+
+  constructor(collection) {
+
+    this._jinq = new jinqJs();
+    this._collection = collection;
+  }
+
+  get chestCollection() {
+
+    return this._jinq.from(this._collection).select();
+  }
+
+  get chestCycle() {
+
+    return this.getChestCycle();
+  }
+
+  getChestCycle() {
+
+    var seqs = "";
+    var values = this._jinq.from(this._collection).select("value");
+
+    for (var i = 0; i < values.length; i++) {
+      seqs += values[i].value;
+    }
+
+    return seqs;
+  }
+
+  getPossibleChestSequences(sequences) {
+
+    var chestSequenceCollection = [];
+
+    for (var i = 0; i < sequences.length; i++) {
+
+      var results = [];
+      var seqs = sequences[i];
+
+      for (var j = 0; j < seqs.length; j++) {
+
+        var seq = seqs[j];
+        var result = this._jinq.from(this._collection).where(function (p) { return p.value === seq; }).top(1).select();
+
+        results.push(result[0]);
+      }
+
+      chestSequenceCollection.push(results);
+    }
+
+    return chestSequenceCollection;
+  }
+}
+
 var vm = new Vue({
   "el": "#app",
   "data": {
@@ -8,50 +62,24 @@ var vm = new Vue({
   },
   "computed": {
     "sequences": function () {
+
       var resultsA = [];
 
       if (this.sequence.length < 3) {
         return resultsA;
       }
 
-      var chestSeqs = "";
-      var chestSeqValues = new jinqJs().from(this.chests).select("value");
-      for (var i = 0; i < chestSeqValues.length; i++) {
-        chestSeqs += chestSeqValues[i].value;
-      }
+      var chest = new Chest(this.chests);
+      var chestCycle = chest.chestCycle;
 
       var inputSeqs = this.sequence.replace(/[XUEL]/g, ".");
 
       var re = new RegExp(inputSeqs, "g");
-      var possibleSeqs = chestSeqs.match(re);
+      var possibleSeqs = chestCycle.match(re);
 
-      for (var i = 0; i < possibleSeqs.length; i++) {
-        var resultsB = [];
-        var seqs = possibleSeqs[i];
-        for (var j = 0; j < seqs.length; j++) {
-          var seq = seqs[j];
-          var result = new jinqJs().from(this.chests).where(function (chest) { return chest.value === seq; }).top(1).select();
-          resultsB.push(result[0]);
-        }
+      var results = chest.getPossibleChestSequences(possibleSeqs);
 
-        resultsA.push(resultsB);
-      }
-
-      return resultsA;
+      return results;
     }
   }
 });
-
-//var sequenceVm = new Vue({
-//  "el": "#chest-sequence",
-//  "data": {
-//    "sequence": ""
-//  }
-//});
-
-//var chestsVm = new Vue({
-//  "el": "#chests",
-//  "data": {
-//    "chests": chests
-//  }
-//});
