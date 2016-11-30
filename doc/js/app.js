026e60f40b17d1c629973c6c268d110a1eaf494a
Justin Yoo <justin.yoo@aliencube.com>
2016-11-30 21:56:44 +1100

    Remove unnecessary files

---
+++ doc/js/app.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+
+var vm = new Vue({
+  "el": "#app",
+  "data": {
+    "sequence": "",
+    "chests": chests
+  },
+  "methods": {
+    "reload": function() {
+      location.reload();
+    }
+  },
+  "computed": {
+    "sequenceCollection": function () {
+
+      var resultsA = [];
+
+      if (this.sequence.length < 3) {
+        return resultsA;
+      }
+
+      var chest = new Chest(this.chests);
+      chest.initialise();
+
+      var chestCycle = chest.chestCycle;
+
+      var inputSeqs = this.sequence.replace(/[XUEL]/g, ".");
+      var re = new RegExp(inputSeqs, "g");
+
+      var match;
+      var possibleSeqs = [];
+      while (match = re.exec(chestCycle)) {
+
+        var seq = { "index": match.index, "value": match[0] };
+        possibleSeqs.push(seq);
+      }
+
+      //var possibleSeqs = chestCycle.match(re);
+
+      var results = chest.getPossibleChestSequences(possibleSeqs);
+
+      return results;
+    }
+  }
+});
---
+++ doc/js/chest.js
@@ -0,0 +1,77 @@
+﻿"use strict";
+
+class Chest {
+
+  constructor(collection) {
+
+    this._jinq = new jinqJs();
+    this._collection = collection;
+    this.initialise();
+  }
+
+  initialise() {
+    this._placeholders = this.getPlaceholders();
+  }
+
+  getPlaceholders() {
+
+    var phs = [];
+    for (var i = 0; i < this._collection.length; i++) {
+      phs.push(" ");
+    }
+
+    return phs;
+  }
+
+  get chestCollection() {
+
+    return this._jinq.from(this._collection).select();
+  }
+
+  get chestCycle() {
+
+    return this.getChestCycle();
+  }
+
+  getChestCycle() {
+
+    var seqs = "";
+    var values = this._jinq.from(this._collection).select("value");
+
+    for (var i = 0; i < values.length; i++) {
+      seqs += values[i].value;
+    }
+
+    return seqs;
+  }
+
+  getPossibleChestSequences(sequences) {
+
+    var chestSequenceCollection = [];
+
+    for (var i = 0; i < sequences.length; i++) {
+
+      var results = [];
+      var index = sequences[i].index;
+      var value = sequences[i].value;
+
+      this._placeholders.splice(index, value.length, value);
+      this._placeholders = this._placeholders.join("").split("");
+
+      for (var j = 0; j < value.length; j++) {
+
+        var selected = this._jinq.from(this._collection).where(function (p) { return p.index === (index + j); }).top(1).select();
+
+        var result = selected[0];
+        result.placeholder = this._placeholders[result.index];
+        result.cssClassPlaceholder = result.cssClass;
+
+        results.push(result);
+      }
+
+      chestSequenceCollection.push({ "index": index, "sequence": results });
+    }
+
+    return chestSequenceCollection;
+  }
+}
